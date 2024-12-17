94de3590cf994325c207a388c7d3bbe01b23ac43
HARSH KUMAR <xt.harshh@gmail.com>
2024-12-17 23:02:13 +0530

    my nextjs auth Jwt based login and signup page  and fixed the error of the next15 and promise error

--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -1,11 +1,8 @@
+
+import React from 'react';
 import VerifyEmailPage from '../verifyemail/VerifyUserEmail';
 
-export async function getServerSideProps(context: { params: { id: string | null; }; }) {
-    const { id } = context.params;
-    return {
-        props: { params: { id } }, // Pass the params directly as props
-    };
-}
+
 
 export default function VerifyEmail({ params }:{params:{id:string | null;}}) {
     return <VerifyEmailPage params={params} />;
